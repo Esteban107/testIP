8a913e1942e75b2a1c1064ed304cae2dca357dae
Esteban107 <31463907+Esteban107@users.noreply.github.com>
2021-01-04 18:17:16 -0600

    Update ping.py

--- ping.py
+++ ping.py
@@ -15,21 +15,16 @@ def ping_ip(current_ip_address):
                 return False
  
 if __name__ == '__main__':
-    """current_ip_address = ['192.168.2.1', '192.168.3.1', '192.168.5.1', '192.168.6.1', '192.168.7.1']"""
 
-    current_ip_address = {'OBISPADO' : '192.168.2.1', 
-                          'PLAZA VITA' : '192.168.3.1', 
-                          'PLANTA 1' : '192.168.5.1', 
-                          'PLANTA 2' : '192.168.6.1', 
-                          'CEDIS' : '192.168.7.1' }  
+    current_ip_address = {'NAMESERVER' : 'DIRECCIONIP', 
+                          'SERVER1' : '192.168.3.100'
+                         }  
     
     for key, value in current_ip_address.items():
         if ping_ip(value):
             print(f"{value} {key} ONLINE ")
-            #print(value , ":", current_ip_address[value])
             
         else:
-             #print(f"{value} : {current_ip_address.keys()} ONLINE")
             print(f"{value} {key}   OFFLINE")
           
           
